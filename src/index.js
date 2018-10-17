38bf494040c95affe5c9a11871f898d62e3227b1
YoonJP <42939727+YoonJP@users.noreply.github.com>
2018-10-17 16:13:35 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,114 @@
-import './index.scss';
+let boardState = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
 
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
+// 이 함수는 상태로부터 화면을 그리는 역할
+function drawBoard() {
+  document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+    rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+      if (boardState[rowIndex][colIndex] === 1) {
+        colEl.classList.add("checked");
+      } else {
+        colEl.classList.remove("checked");
+      }
+    });
+  });
+
+  if (bingo(boardState)) {
+    document.querySelector(".result").textContent = "빙고!";
+    document.querySelector(".reset").classList.add("show");
+  } else {
+    document.querySelector(".result").textContent = "";
+    document.querySelector(".reset").classList.remove("show");
+  }
+}
+
+// 클릭할 때 일어나는 변화(한 칸씩 체크)
+document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+  rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+    colEl.addEventListener("click", e => {
+      if (!bingo(boardState)) {
+        boardState[rowIndex][colIndex] = 1;
+        drawBoard();
+      }
+    });
+  });
 });
+
+drawBoard();
+
+// 리셋 버튼(다시 시작)
+document.querySelector(".reset").addEventListener("click", e => {
+  // alert("asdf"); // 이벤트리스터 작동 확인용 코드
+  boardState = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+  ];
+  drawBoard();
+});
+
+// 빙고 함수
+function bingo(arr) {
+  // 가로줄 확인 (루프)
+  for (let i = 0; i < 5; i++) {
+    // '이제까지 본 것이 전부 x표시가 되어있다'
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[i][j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  // 세로줄 확인 (루프)
+  for (let i = 0; i < 5; i++) {
+    // '이제까지 본 것이 전부 x표시가 되어있다'
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][i] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  {
+    // 대각선 확인 (루프)
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  {
+    // 반대쪽 대각선 확인 (루프)
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][4 - j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  return false;
+}
